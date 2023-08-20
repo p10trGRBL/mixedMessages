728fdee193e3fcb3c0b90cdd5feffe1634f7f6c9
Piotr Groblewski <groblewskip@gmail.com>
2023-08-20 19:29:39 +0200

    working version

--- script.js
+++ script.js
@@ -8,68 +8,41 @@
 
 // create an array of pics
 
-let picBase = [];
- 
-const pic1 = new Image();
-
-pic1.src='imagesMixMess/meme1.jpeg';
-pic1.alt='meme1';
-
-const pic2 = new Image();
-pic1.src='imagesMixMess/meme2.jpeg';
-pic1.alt='meme2';
-
-const pic3 = new Image();
-pic1.src='imagesMixMess/meme3.jpeg';
-pic1.alt='meme3';
-
-const pic4 = new Image();
-pic1.src='imagesMixMess/meme4.jpeg';
-pic1.alt='meme4';
-
-const pic5 = new Image();
-pic1.src='imagesMixMess/meme5.jpeg';
-pic1.alt='meme5';
-
-const pic6 = new Image();
-pic1.src='imagesMixMess/meme6.jpeg';
-pic1.alt='meme6';
-
-const pic7 = new Image();
-pic1.src='imagesMixMess/meme7.jpeg';
-pic1.alt='meme7';
-
-const pic8 = new Image();
-pic1.src='imagesMixMess/meme8.jpeg';
-pic1.alt='meme8';
-
-const pic9 = new Image();
-pic1.src='imagesMixMess/meme9.jpeg';
-pic1.alt='meme9';
-
-const pic10 = new Image();
-pic1.src='imagesMixMess/meme10.jpeg';
-pic1.alt='meme10';
-
-const pic11 = new Image();
-pic1.src='imagesMixMess/meme11.jpeg';
-pic1.alt='meme11';
-
-picBase.push(pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11);
+let picBase = ['imagesMixMess/meme1.jpeg','imagesMixMess/meme2.jpeg','imagesMixMess/meme3.jpeg','imagesMixMess/meme4.jpeg','imagesMixMess/meme5.jpeg','imagesMixMess/meme6.jpeg','imagesMixMess/meme7.jpeg','imagesMixMess/meme8.jpeg','imagesMixMess/meme9.jpeg','imagesMixMess/meme10.png','imagesMixMess/meme11.png'];
 
 //choosing random element from picBase array
-//adding the chosen element to the document
-
-document.querySelector("duckButton").addEventListener('click', addPic());
 
 function addPic() { 
-    let randomNumber = Math.round(Math.random()*10);
-    let currentPic;
-    currentPic = picBase[randomNumber];
-    let addedDiv = document.querySelector('memeContainer').createElement('div');
-    addedDiv.append(currentPic);
-
+    let background = document.getElementById('memeScene');
+    let randomNumber = Math.floor(Math.random()*11);
+    let displayedPic;
+    let theDiv;
+    let thePic
+    let currentPicSrc = picBase[randomNumber];
+    if (background.style.display === 'none'){
+    background.style.display = 'flex';
+    background.style.alignItems = 'center';
+    displayedPic = document.createElement('img');
+    displayedPic.src=`${currentPicSrc}`;
+    displayedPic.id='addedPic'
+    displayedPic.style.width= '400px';
+    displayedPic.style.height= 'auto';
+    theDiv=document.getElementById('memeScene');
+    theDiv.appendChild(displayedPic);
+    } else {
+        background.style.display='none';
+        thePic=document.getElementById('addedPic');
+        thePic.remove();
+    }
+    
 }
+
+//adding the chosen element to the document
+//console.log(addPic())
+let guzik = document.getElementById('duckButton');
+guzik.addEventListener('click', addPic);
+
+
 
 //adding alerts for empty submission fields
 
@@ -77,5 +50,5 @@ function alertOnEmpty() {
     if (document.getElementById('textBox').value === "" || document.getElementById('textBox').value === " ") {
         alert("Not so fast cowboy! You forgot to type the message");
     }
-};
+}
 document.querySelector('#formButton').addEventListener('click', alertOnEmpty);
